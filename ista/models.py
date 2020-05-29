c7ff6d9c92786f7bf6c01714a847024ea0f3f42d
barackmaund1 <pmaundu49@gmail.co>
2020-05-30 02:32:51 +0300

    created superuser

--- ista/models.py
+++ ista/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image_name=models.CharField(max_length=50)
+    image=image=models.ImageField(upload_to= 'images/')
+    caption = models.CharField(max_length=250, blank=True)
+

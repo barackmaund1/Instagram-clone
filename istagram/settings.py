e1cb4de199c56d6f44b35548c72f67e94d8c7e78
barackmaund1 <pmaundu49@gmail.co>
2020-05-30 04:52:14 +0300

    users form

--- istagram/settings.py
+++ istagram/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'ista',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']

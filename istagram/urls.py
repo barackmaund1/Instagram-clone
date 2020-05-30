8b8308ae5c46a7b804e0f2bf74ea34cc6bef0b4c
barackmaund1 <pmaundu49@gmail.co>
2020-05-30 09:32:50 +0300

    specified templates for login and logout

--- istagram/urls.py
+++ istagram/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('ista.urls')),
     path('register/',user_views.register,name='register'),
-    path('login/',auth_views.LoginView.as_view(),name='login'),
-    path('login/',auth_views.Logout.as_view(),name='logout'),
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('login/',auth_views.Logout.as_view(template_name='users/log'),name='logout'),
 ]

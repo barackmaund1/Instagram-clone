b995902d6b6c24ac4de075acea4eae4582f9556a
barackmaund1 <pmaundu49@gmail.co>
2020-05-30 10:23:02 +0300

    profile model

--- users/models.py
+++ users/models.py
@@ -1,12 +1,24 @@
 from django.db import models
-from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 
 
-class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+from django.db import models
+from django.contrib.auth.models import User
 
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    bio=models.CharField(max_length=100)
+    email=models.EmailField()
+    
+    @classmethod
+    def search_user(cls,username):
+        return User.objects.filter(username__icontains = username)
+
+    def save_profile(self):
+        self.save()
+    def __str__(self):
+        return f'{self.user.username} Profile'
+

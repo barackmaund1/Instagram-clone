1daa65022dfec20b6fd289c2ba1a7d4043696751
barackmaund1 <pmaundu49@gmail.co>
2020-05-30 10:41:49 +0300

    specified media root

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import UserRegisterForm
+from .forms import UserRegisterForm
 # Create your views here.
 
 def register(request):

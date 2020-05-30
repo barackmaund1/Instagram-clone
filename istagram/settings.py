02d78823e05ace775c8cf967fb0317b6eaf9881e
barackmaund1 <pmaundu49@gmail.co>
2020-05-30 06:01:26 +0300

    installed crispy_form

--- istagram/settings.py
+++ istagram/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'ista',
     'users',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'

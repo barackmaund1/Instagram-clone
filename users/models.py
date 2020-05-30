525ebc60fa160f5217965f8473809068077afef9
barackmaund1 <pmaundu49@gmail.co>
2020-05-30 12:40:37 +0300

    TestProfile test class

--- users/models.py
+++ users/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.png', upload_to='profile_pics')
+    image = models.ImageField(default='default.png', upload_to='profile_pics/')
     bio=models.CharField(max_length=100)
-    email=models.EmailField()
+    
     
     @classmethod
     def search_user(cls,username):
@@ -16,3 +16,12 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
-
+from .models import Profile
 # Create your tests here.
+from django.contrib.auth.models import User
+
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(username='barack')
+        self.user.save()
+
+        self.profile_test = Profile(id=1, image='default.jpg', bio='this is a test profile',
+                                    user=self.user)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile_test, Profile))
+
+    def test_save_profile(self):
+        self.profile_test.save_profile()
+        after = Profile.objects.all()
+        self.assertTrue(len(after) > 0)
